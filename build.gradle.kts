7971b7d69d8b17047cb3544478048697b77e6ab4
Johnny Carreiro <johnny@johnnycarreiro.com>
2026-06-08 14:55:09 -0300

    chore(main): release 0.3.0 (#17)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.johnnycarreiro.hybridauth"
-    version = "0.2.0" // x-release-please-version
+    version = "0.3.0" // x-release-please-version
 
     repositories {
         mavenCentral()
